cc63f1d4df8d96dc78d698c826167da4a120cd5d
ZuniAmandaDewi <zuniamanda06@gmail.com>
2022-03-09 19:45:48 +0700

    menambahkan file fungsi.php

---
+++ fungsi.php
@@ -0,0 +1,13 @@
+<?php
+function penjumlahan($angka1, $angka2) {
+	$hasil = $angka1 + $angka2;
+	return $hasil;
+}
+
+echo penjumlahan (7,8);
+echo "<br>";
+echo penjumlahan (8,0);
+echo "<br>";
+echo penjumlahan (1,2);
+echo "<br>";
+?>
