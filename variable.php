97f14dccc41e25cc069ea892bf8f82c5c533eb87
ZuniAmandaDewi <zuniamanda06@gmail.com>
2022-03-09 19:46:44 +0700

    menambahkan file variable.php

---
+++ variable.php
@@ -0,0 +1,16 @@
+<?php
+	$angka1 = 6;
+	$angka2 = 2;
+
+	//pejumlahan
+	$penjumlahan = $angka1 + $angka2;
+	echo "penjumlahan ".$angka1. " + ". $angka2. " adalah = ". $penjumlahan;
+
+	//pengurangan
+	$pengurangan = $angka1 - $angka2;
+	echo "penjumlahan ".$angka1. " - ". $angka2. " adalah = ". $pengurangan;
+
+	//perkalian
+	$perkalian = $angka1 * $angka2;
+	echo "penjumlahan ".$angka1. " * ". $angka2. " adalah = ". $perkalian;
+?>
