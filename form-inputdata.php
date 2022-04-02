a14540a1a768b94dbb55aee0aa1c458428c0911d
ZuniAmandaDewi <101152490+ZuniAmandaDewi@users.noreply.github.com>
2022-04-02 18:57:43 +0700

    code form input (Tugas 3)

---
+++ form-inputdata.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Input Data</title>
+</head>
+<body>
+	<h2>FORM INPUT DATA</h2>
+	<form method="POST" action="aksi.php">
+		<table border="0">
+			<tr>
+				<td><label for="Kode">Kode Provinsi:</label></td>
+				<td><input id="kode" name="kode" type="text"/></td>
+			</tr>
+			<tr>
+				<td><label for="nama">Nama Provinsi:</label></td>
+				<td><input id="nama" name="nama" type="text"/></td>
+			</tr>
+			<tr>
+				<td><input type="submit" value="submit"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
